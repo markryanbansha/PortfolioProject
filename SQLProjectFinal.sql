a0b1448dc33907039e91bed75ddf5e423330db08
Mark Ryan <markryan2010@gmail.com>
2024-10-06 10:25:14 +0100

    Add files via upload

---
+++ SQLProjectFinal.sql
@@ -0,0 +1,11 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[RollingPeopleVaccinated]
+  FROM [PortfolioProject].[dbo].[PercentPopulationVaccinated]
+
+
+  SELECT * 
+  FROM PercentPopulationVaccinated
